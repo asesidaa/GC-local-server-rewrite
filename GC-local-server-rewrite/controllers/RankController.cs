47308466742a1bfc9fa4cef6ce4f4ab14873e420
Yuchen Ji <jiych1@shanghaitech.edu.cn>
2022-06-10 00:48:03 +0800

    Add random play_num_rank for testing

--- GC-local-server-rewrite/controllers/RankController.cs
+++ GC-local-server-rewrite/controllers/RankController.cs
@@ -7,6 +7,7 @@ using EmbedIO.Routing;
 using EmbedIO.WebApi;
 using GCLocalServerRewrite.common;
 using GCLocalServerRewrite.models;
+using SQLite.Net2;
 using Swan;
 using Swan.Logging;
 // ReSharper disable UnusedMember.Global
@@ -15,6 +16,16 @@ namespace GCLocalServerRewrite.controllers;
 
 public class RankController : WebApiController
 {
+    private readonly SQLiteConnection cardSqLiteConnection;
+    private readonly SQLiteConnection musicSqLiteConnection;
+
+    public RankController()
+    {
+        cardSqLiteConnection = DatabaseHelper.ConnectDatabase(Configs.SETTINGS.CardDbName);
+        musicSqLiteConnection = DatabaseHelper.ConnectDatabase(Configs.SETTINGS.MusicDbName);
+    }
+
+    
     [Route(HttpVerbs.Get, "/ranking.php")]
     public string Rank([QueryField("cmd_type")] int type)
     {
@@ -24,7 +35,7 @@ public class RankController : WebApiController
 
         if (!Enum.IsDefined(typeof(RankType), type))
         {
-            throw new ArgumentOutOfRangeException(nameof(type));
+            throw new ArgumentOutOfRangeException(nameof(type), type, "Rank type out of range");
         }
 
         var requestType = (RankType)type;
@@ -41,7 +52,7 @@ public class RankController : WebApiController
             case RankType.PlayNumRank:
                 $"Getting rank request, type is {requestType}".Info();
 
-                return ConstructResponse(RankTemp("play_num_rank"));
+                return ConstructResponse(PlayNumRank());
             case RankType.EventRank:
                 $"Getting rank request, type is {requestType}".Info();
 
@@ -96,6 +107,63 @@ public class RankController : WebApiController
 
         return stringWriter.ToString();
     }
+
+    private string PlayNumRank()
+    {
+        var rankStatus = new RankStatus
+        {
+            Rows = 30,
+            TableName = "play_num_rank",
+            Status = 1
+        };
+
+        var playNumRankContainer = new PlayNumRankContainer
+        {
+            PlayNumRankRecords = GetPlayNumRankRecords(),
+            RankStatus = rankStatus
+        };
+
+        var sb = new StringBuilder();
+
+        using (var writer = new ChoXmlWriter<PlayNumRankContainer>(sb))
+        {
+            writer.Configuration.UseXmlSerialization = true;
+            writer.Configuration.OmitXmlDeclaration = false;
+            writer.Configuration.IgnoreRootName = true;
+            writer.Write(playNumRankContainer);
+        }
+
+        return sb.ToString();
+    }
+
+    private List<PlayNumRankRecord> GetPlayNumRankRecords()
+    {
+        var records = new List<PlayNumRankRecord>();
+        var musics = musicSqLiteConnection.Table<Music>().ToList().OrderBy(arg => Guid.NewGuid()).Take(30).ToList();
+
+        for (var i = 0; i < musics.Count; i++)
+        {
+            var music = musics[i];
+            var index = i + 1;
+            var record = new PlayNumRankRecord
+            {
+                Id = index,
+                Rank = index,
+                Rank2 = index + 1,
+                PrevRank = musics.Count - i,
+                PrevRank2 = index + 1,
+                Artist = music.Artist,
+                Pcol1 = music.MusicId,
+                ScoreBi1 = index,
+                Title = music.Title
+            };
+            records.Add(record);
+        }
+
+
+        return records;
+    }
+
 
     private enum RankType
     {
--- GC-local-server-rewrite/models/RankStatus.cs
+++ GC-local-server-rewrite/models/RankStatus.cs
@@ -2,16 +2,17 @@
 
 namespace GCLocalServerRewrite.models;
 
+[XmlType("ranking_status")]
 public class RankStatus
 {
     [XmlElement("table_name")]
     public string? TableName { get; set; }
 
     [XmlElement("start_date")]
-    public string StartDate { get; set; } = "2017-05-30";
+    public string StartDate { get; set; } = "2021-05-30 08:00:00";
 
     [XmlElement("end_date")]
-    public string EndDate { get; set; } = "2018-05-30";
+    public string EndDate { get; set; } = "2022-06-08 08:00:00";
 
     [XmlElement("status")]
     public int Status { get; set; }
